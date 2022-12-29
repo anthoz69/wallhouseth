8e3d7409e0ff3ec03b6c4001440755a2614bc4c8
anthoz69 <st.anthoz69@gmail.com>
2022-12-29 20:18:57 +0700

    fix: path

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -46,7 +46,7 @@
 <form id="logoutform" action="{{ route('logout') }}" method="POST" style="display: none;">
     {{ csrf_field() }}
 </form>
-<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
 <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
 @livewireScripts
 @yield('scripts')
