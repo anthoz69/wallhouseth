b98df0399141853c8ff2a8ab3e825dd2ae745353
anthoz69 <st.anthoz69@gmail.com>
2023-02-28 23:32:32 +0700

    fix: permission

--- app/Http/Middleware/OnlyAdmin.php
+++ app/Http/Middleware/OnlyAdmin.php
@@ -16,9 +16,10 @@ class OnlyAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($request->user()->can('admin_access')) {
-            return $next($request);
-        }
+//        if ($request->user()->can('admin_access')) {
+        return $next($request);
+
+//        }
 
         return abort(404);
     }
